fd78db1efa9beab22da2bd2edecede433295e4f0
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-04-14 08:46:16 -0500

    SMS sending is working yaay but with this limitations:
    - In this moment we need to take the devise token from logs
    - Then we need to include that token in the request manually
    - The cool thing is that the sms is send base on the http request
      from Firebase
    
    Next steps:
    - Create a new activity screen to set ApiKey
    - After clicking button we need to send ApiKey and devise token to
      our API, validate information
    - Save information in a shared preference value
    - Move user to next page about showing that the APIkey and token device
      was saved successfully and that it is ready to process SMS messages
    - In case we want to test this functionality, we go to the server console
      And we create a new SMS notification record with a valid number and message
      And we just need to wait to receive that SMS in our device.

--- app/src/main/java/com/example/smsmobilehub/MainActivity.java
+++ app/src/main/java/com/example/smsmobilehub/MainActivity.java
@@ -1,25 +1,103 @@
 package com.example.smsmobilehub;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "Noticias";
+    public static final String TAG = "SmsMobileHubActivity";
+    private TextView infoTextView;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String token = FirebaseInstanceId.getInstance().getToken();
 
-        Log.d(TAG, "Token: " + token);
+        FirebaseApp.initializeApp(this);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        String instanceId = instanceIdResult.getId();
+                        Log.d(TAG, "InstanceId: " + instanceId);
+                    }
+                });
+
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        String newToken = instanceIdResult.getToken();
+                        Log.e("newToken",newToken);
+
+                    }
+                });
+
+        //testingCode();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+                Log.d("primero", "The onCreate() event");
+
+            } else {
+                Log.d("segundo", "The onCreate() event");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+
+        } else {
+            Log.d("granteeed", "The onCreate() event");
+        }
+    }
+
+    private String testingCode()  {
+        String input = "123456789";     //input string
+        Log.d("veamos", input);
+
+        return input;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Log.d("onRequest", "The onCreate() event");
+
+        String msgPermissionGranted = "permission granted";
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(msgPermissionGranted, "The onCreate() event");
+//                    sendSmsCompi();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/example/smsmobilehub/MyFireBaseMessagingService.java
@@ -0,0 +1,163 @@
+package com.example.smsmobilehub;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import androidx.core.app.NotificationCompat;
+
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class MyFireBaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        super.onMessageReceived(remoteMessage);
+        String from = remoteMessage.getFrom();
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Data: " + remoteMessage.getData());
+            Map<String, String> processedData = remoteMessage.getData();
+            String cellPhoneNumber = processedData.get("cellphone_number");
+            String smsContent = processedData.get("sms_content");
+
+            Log.d(TAG, "cellPhoneNumber: " + cellPhoneNumber);
+            Log.d(TAG, "smsContent: " + smsContent);
+
+            if(cellPhoneNumber.isEmpty() || smsContent.isEmpty()) {
+                return;
+            }
+
+            sendSMS(cellPhoneNumber, smsContent);
+
+            // here we will trigger the SMS service and pass the values so this could be triggered
+            // and once the sms is sent successfully we would request to our api
+            // informing about the sent was successfully did or in case it failed
+        }
+    }
+
+    private void showNotificacion(String title, String body) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+    private void sendSMS(String phoneNumber, String message)
+    {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        final String finalPhoneNumber = phoneNumber;
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS enviado a " + decoratePhoneNumberDigits(finalPhoneNumber),
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+    }
+
+    private String decoratePhoneNumberDigits(String phoneNumber)  {
+        String lastFourDigits = "";     //substring containing last 4 characters
+
+        String firstDigits = new String();
+        for(int i = 0; i < phoneNumber.length() - 4; i++) {
+            char c = phoneNumber.charAt(i);
+            firstDigits += "*";
+        }
+
+        if (phoneNumber.length() > 4) {
+            lastFourDigits = phoneNumber.substring(phoneNumber.length() - 4);
+        } else {
+            lastFourDigits = phoneNumber;
+        }
+
+        for(int i = 0; i < lastFourDigits.length(); i++)
+        {
+            char c = lastFourDigits.charAt(i);
+            firstDigits += c;
+        }
+
+        return firstDigits;
+    }
+}
